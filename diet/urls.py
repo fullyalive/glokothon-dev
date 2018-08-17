a5450968b610b713bd1f713b7f7e6fbd17218d8c
Jieun Kim <wwooww374@gmail.com>
2018-08-17 20:22:18 +0900

    회원가입

---
+++ diet/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^signup/$', views.signup, name='signup'),
+]
+
+
+
+# 웹사이트 화면   --- >   웹 서버 (채팅서버)    --- > dialogflow (질문, 답변)
+
+# [인텐트] 운동법요청
+#  (다리 운동법) entity1 알려줘 
+#  (가슴 운동법) entity2 알려줘
+# 
